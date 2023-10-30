0860d71c8ffeed8eb122e94d376d136790b1ff13
ShubhangiMahale3001 <mahaleshubhangi82@gmail.com>
2023-10-30 23:17:33 +0530

    Initial commit

---
+++ simpleCalculator/src/simpleCalculator/SimpleCalculator.java
@@ -0,0 +1,70 @@
+package simpleCalculator;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter your choice (1-5): ");
+            int choice = scanner.nextInt();
+
+            if (choice == 5) {
+                System.out.println("Exiting the calculator.");
+                break;
+            }
+
+            double num1, num2;
+            System.out.print("Enter the first number: ");
+            num1 = scanner.nextDouble();
+            System.out.print("Enter the second number: ");
+            num2 = scanner.nextDouble();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result: " + add(num1, num2));
+                    break;
+                case 2:
+                    System.out.println("Result: " + subtract(num1, num2));
+                    break;
+                case 3:
+                    System.out.println("Result: " + multiply(num1, num2));
+                    break;
+                case 4:
+                    if (num2 != 0) {
+                        System.out.println("Result: " + divide(num1, num2));
+                    } else {
+                        System.out.println("Error: Division by zero!");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+}
